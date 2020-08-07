44957219ed034bb1808bb9e27aefa9a451a0a6b2
Victor Lliuya <alonso.victor38@gmail.com>
2020-08-06 23:10:09 -0500

    Notebooks v1

---
+++ notebooks/jenkinsfile-notebooks.groovy
@@ -0,0 +1,83 @@
+/* Project 4 letters. */ 
+def project                 = 'BMDL'
+def deploymentEnvironment   = 'dev'
+def appName                 = "databricks-cli";
+def appVersion              = "2.0";
+def AzureResourceName       = "prueba";
+def azureWebApp             = "demo-app-inct";
+def dockerRegistryUrl       = "vlliuyadesa.azurecr.io";
+def imageTag                = "${dockerRegistryUrl}/${appName}:${appVersion}";
+def databricksHost          = "https://eastus2.azuredatabricks.net";
+def databricksContainer     = "";
+
+/* Mail configuration*/
+// If recipients is null the mail is sent to the person who start the job
+// The mails should be separated by commas(',')
+//def recipients
+
+try {
+   node { 
+      stage('Pull Source Code') {
+        checkout([$class: 'GitSCM', branches: [[name: '*/master']], 
+        doGenerateSubmoduleConfigurations: false, 
+        extensions: [], 
+        submoduleCfg: [], 
+        userRemoteConfigs: [[credentialsId: 'github-jenkins', 
+        url: 'https://github.com/alonsoTI/PRTI-Databricks.git']]])
+      }
+
+      stage('Prepare Notebooks') {
+        steps.echo """
+        ******** Read Notebooks  ********
+        """
+        steps.sh "cd notebooks"
+        steps.sh "ls"
+      }
+
+      stage('QA Analisys') {
+        steps.echo """
+        ******** Analiza con Sonar ********
+        """
+      }
+     
+      stage('SAST Analisys') {
+        steps.echo """
+        ******** Analiza con Fortify********
+        """
+      }
+
+      stage('Save Artifact') {
+        steps.echo """
+        ******** Almacena en Artifactory ********
+        """
+      }
+
+      stage("Deploy to " +deploymentEnvironment){
+
+        steps.echo """
+        ******** Importando notebooks en Databricks DEV********
+        """
+  
+        steps.withCredentials([
+                [$class: "StringBinding", credentialsId: "tenantId", variable: "tenantId" ],
+                [$class: "StringBinding", credentialsId: "databricksToken", variable: "databricksToken" ]
+            ]){
+                try{
+                    
+                     CLUSTER = readJSON text: sh("cat notebook.json")
+
+                }catch(Exception e){
+                throw e;
+                }
+            }
+      }
+
+      stage('Post Execution') {
+        
+      }
+   }
+} catch(Exception e) {
+   node {
+    throw e
+   }
+}
